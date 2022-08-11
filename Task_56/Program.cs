e9a6f248a9c345f9ec17e6b10f3c802b10534363
Dmitriy <Fes08-09@yandex.ru>
2022-08-11 18:59:39 +0300

    Homework_sem_10_done

---
+++ Task_56/Program.cs
@@ -0,0 +1,102 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим
+// следующий массив:
+// 9 4 2
+// 2 2 6
+// 3 4 7
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число строк массива: ");
+int m = int.Parse(ReadLine());
+WriteLine("Введите число столбцов массива: ");
+int n = int.Parse(ReadLine());
+WriteLine("Введите диапазон значений элементов массива: от 1 до ");
+int range = int.Parse(ReadLine());;
+
+int[,] array = new int[m, n];
+GetNewArray(array, range);
+PrintArray(array);
+
+int[,] positionSmallElement = new int[1, 2];
+positionSmallElement = FindPositionSmallElement(array, positionSmallElement);
+Write($"Позиция элемента: ");
+PrintArray(positionSmallElement);
+
+int[,] arrayWithoutLines = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+DeleteLines(array, positionSmallElement, arrayWithoutLines);
+Console.WriteLine($"Получившийся массив:");
+PrintArray(arrayWithoutLines);
+
+
+void GetNewArray(int[,] array, int range1)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range1);
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Write(array[i, j] + " ");
+    }
+    WriteLine();
+  }
+}
+
+int[,] FindPositionSmallElement(int[,] array, int[,] position)
+{
+  int temp = array[0, 0];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i, j] <= temp)
+      {
+        position[0, 0] = i;
+        position[0, 1] = j;
+        temp = array[i, j];
+      }
+    }
+  }
+  WriteLine($"Mинимальный элемент: {temp}");
+  return position;
+}
+
+void DeleteLines(int[,] array, int[,] positionSmallElement, int[,] arrayWithoutLines)
+{
+  int k = 0; 
+  int l = 0;
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (positionSmallElement[0, 0] != i && positionSmallElement[0, 1] != j)
+      {
+        arrayWithoutLines[k, l] = array[i, j];
+        l++;
+      }
+    }
+    l = 0;
+    if (positionSmallElement[0, 0] != i)
+    {
+      k++;
+    }
+  }
+}
---
+++ Task_58/Program.cs
@@ -0,0 +1,70 @@
+﻿// Дополнительная задача 61: Показать треугольник Паскаля. Сделать вывод в виде равнобедренного треугольника
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите количество строк: ");
+int n = int.Parse(ReadLine());
+
+double[,] pascalTriangle = new double[n + 1, 2 * n + 1];
+
+FillPascalTriangle(pascalTriangle);
+
+WriteLine();
+
+TransformationPascalTriangle(pascalTriangle);
+
+WriteLine();
+PrintArray(pascalTriangle);
+
+void TransformationPascalTriangle(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    int count = 0;
+    for (int j = array.GetLength(1) - 1; j >= 0; j--)
+    {
+      if (array[i, j] != 0)
+      {
+        array[i, array.GetLength(1) / 2 + j - count] = array[i, j];
+        array[i, j] = 0;
+        count++;
+      }
+    }
+  }
+  array[array.GetLength(0) - 1, 0] = 1;
+}
+
+void FillPascalTriangle(double[,] array)
+{
+  for (int k = 0; k < array.GetLength(0); k++)
+  {
+    array[k, 0] = 1;
+  }
+  for (int i = 1; i < array.GetLength(0); i++)
+  {
+    for (int j = 1; j < i + 1; j++)
+    {
+      array[i, j] = array[i - 1, j] + array[i - 1, j - 1];
+    }
+  }
+}
+
+void PrintArray(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i, j] != 0)
+      {
+          Write($"{array[i, j]} ");
+      }
+      else Write("  ");
+    }
+    WriteLine();
+  }
+}
+
